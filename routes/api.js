562ff91540bbd23d92c38a5fad12611756a23185
Zi-on <zioniflores12@yahoo.com>
2021-09-07 23:29:27 -0500

    fixed my api post route and added a get and post for workouts/range

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,56 @@
 const router = require('express').Router();
-const db = require('../models');
+const Workout = require('../models/workout.js');
 
-router.get('/api/workout', (req, res) => {
-    db.Workout.find({})
+router.get('/api/workouts', ({body}, res) => {
+    Workout.find(body)
    .then(function(dbWorkout) {
      res.json(dbWorkout);
    })
 });
 
-router.post("/api/workout", (req, res) => {
-    db.Workout.create(req.body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+      .then(data => {
+        res.json(data);
       })
       .catch(err => {
         res.status(400).json(err);
       });
   });
+
+router.put("/api/workouts/:id", ({body,params}, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push:{exercises:body} }
+  )
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+  .then(function (data) {
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  })
+});
+
+router.post("/api/workouts/range", ({body}, res) => {
+  Workout.create(body)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
 
 module.exports = router
